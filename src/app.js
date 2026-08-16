b486449d96f7d49c241fe8af745592c85c3471e4
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-08-16 22:33:56 +0500

    add helmet and morgan middleware for security and logging

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import logger from './utils/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
@@ -9,6 +11,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
+app.use(helmet());
+app.use(
+  morgan('combined', {
+    stream: {
+      write: message => {
+        logger.info(message.trim());
+      },
+    },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Hello from acquisition API');
