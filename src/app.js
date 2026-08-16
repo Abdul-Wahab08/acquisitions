37ebb2dcaf26c6b3b717292e70dce331a2fe85d5
Abdul-Wahab08 <abdulwahab3929706@gmail.com>
2026-08-16 16:20:03 +0500

    add eslint and prettier for formatting

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import express from "express"
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express from 'express';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
 
-export default app
+export default app;
